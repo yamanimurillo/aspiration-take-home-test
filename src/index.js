c386acecfabe24c7e495d594a2078bd2c88c1b95
yamanimurillo <yamani.murillo@gmail.com>
2022-03-31 02:26:12 -0600

    Quick fix for api keys

--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@ import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloClient } from 'apollo-boost';
 
+const token = `${process.env.REACT_APP_API_KEY1}${process.env.REACT_APP_API_KEY2}`;
+
 const httpLink = createHttpLink({
   uri: 'https://api.github.com/graphql',
-  headers: { authorization: `Bearer ${process.env.REACT_APP_API_KEY}` }
+  headers: { authorization: `Bearer ${token}` }
 });
 
 export const client = new ApolloClient({
