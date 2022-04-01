cf199f89081597288413ce64ce191850b3435b9a
yamanimurillo <yamani.murillo@gmail.com>
2022-03-31 22:09:10 -0600

    Add title on results explaining that are related topics

--- src/components/related-topics-results/related-topics-results.component.jsx
+++ src/components/related-topics-results/related-topics-results.component.jsx
@@ -9,7 +9,7 @@ const Loader = () => (
         <ThreeDots heigth="100" width="100" color="#014F86" ariaLabel="loading-indicator" />
     </div>);
 
-const Error = ({ error }) =>  <div>{error.message.toString()}</div>;
+const Error = ({ error }) => <div>{error.message.toString()}</div>;
 
 const RelatedTopicsResults = ({ searchTopic, onTopicSelected }) => {
 
@@ -28,16 +28,24 @@ const RelatedTopicsResults = ({ searchTopic, onTopicSelected }) => {
                                     if (loading) return <Loader></Loader>
                                     if (error) return <Error error={error}></Error>
 
+
+                                    console.log(data);
                                     return (
 
                                         <div>
+
                                             {
                                                 data.topic && data.topic.relatedTopics.length > 0 ?
-                                                    data.topic.relatedTopics.map(topic => {
-                                                        return (
-                                                            <RelatedTopic key={topic.id} topic={topic} onTopicSelected={onTopicSelected} />
-                                                        )
-                                                    }) :
+                                                    <div>
+                                                        <span>Related topics</span>
+                                                        {
+                                                            data.topic.relatedTopics.map(topic => {
+                                                                return (
+                                                                    <RelatedTopic key={topic.id} topic={topic} onTopicSelected={onTopicSelected} />
+                                                                )
+                                                            })
+                                                        }
+                                                    </div> :
                                                     <span>
                                                         Whoooaaa! I couldn't find any results.
                                                         <br></br>
